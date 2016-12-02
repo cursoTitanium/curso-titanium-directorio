eb50b5a3002b4ab2bf1a5ae214ed4178ea4c3f32
Jorge Macías <jormagar@upvnet.upv.es>
2016-12-02 10:22:33 +0100

    Alternativa si no hay app de mapas

--- app/controllers/detalle.js
+++ app/controllers/detalle.js
@@ -39,7 +39,12 @@ function accionEmail(e){
  * @description Abrimos la aplicación de mapa
  */
 function accionMapa(e){
-	Ti.Platform.openURL("geo:0,0?q=" + usuario.location.city);
+	var result = Ti.Platform.openURL("geo:0,0?q=" + encodeURIComponent(usuario.location.city));
+	
+	//No tenemos una aplicación que abra este esquema de url
+	if(!result){
+		Ti.Platform.openURL("https://www.google.com/maps?q=" + encodeURIComponent(usuario.location.city));
+	}
 }
 
 /**
